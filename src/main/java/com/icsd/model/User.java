fad59b684f1aaafd7156d6d9b0b8542d4c1512cb
AnkuGaba <66530250+AnkuGaba@users.noreply.github.com>
2020-06-15 10:28:58 +0530

    Update User.java

--- src/main/java/com/icsd/model/User.java
+++ src/main/java/com/icsd/model/User.java
@@ -4,17 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import lombok.ToString;
-//
-//@Getter
-//@Setter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//@ToString
 @Entity
 public class User {
 	@Id
